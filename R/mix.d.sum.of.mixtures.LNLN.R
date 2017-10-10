40865a205a17571e4298e18d4ff540f64a2a49ff
lisaamrhein <lisa.amrhein@helmholtz-muenchen.de>
2017-10-10 15:00:03 +0200

    mischungs Density erstelle (density eines gemischten Vektors)

---
+++ R/mix.d.sum.of.mixtures.LNLN.R
@@ -0,0 +1,7 @@
+mix.d.sum.of.mixtures.LNLN <- function(y, n.vector, p.vector, mu.vector, sigma.vector, logdens = T){
+    densmix <- matrix(0, ncol = max(y), nrow = length(n.vector))
+    for(i in 1: length(n.vector)){
+        densmix[i, ] <- d.sum.of.mixtures.LNLN(y = y, n = n.vector[i], p.vector = p.vector, mu.vector = mu.vector, sigma.vector = sigma.vector, logdens = logdens)
+    }
+    Dens<-colSums(densmix)/length(n.vector)
+}
