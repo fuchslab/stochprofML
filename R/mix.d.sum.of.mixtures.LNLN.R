844cbfb353e0a38a242549d7af7305a11b091e0a
lisaamrhein <lisa.amrhein@helmholtz-muenchen.de>
2018-05-07 09:18:16 +0200

    StochprofML2 to StochprofML

--- R/mix.d.sum.of.mixtures.LNLN.R
+++ R/mix.d.sum.of.mixtures.LNLN.R
@@ -1,5 +1,5 @@
 mix.d.sum.of.mixtures.LNLN <- function(y, n.vector, p.vector, mu.vector, sigma.vector, logdens = T){
-    densmix <- matrix(0, ncol = max(y), nrow = length(n.vector))
+    densmix <- matrix(0, ncol = length(y), nrow = length(n.vector))
     for(i in 1: length(n.vector)){
         densmix[i, ] <- d.sum.of.mixtures.LNLN(y = y, n = n.vector[i], p.vector = p.vector, mu.vector = mu.vector, sigma.vector = sigma.vector, logdens = logdens)
     }
